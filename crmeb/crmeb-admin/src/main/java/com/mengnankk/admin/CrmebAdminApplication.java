36e6440576dc406f3f9c75ac53df1ad742815837
mengnankkkk <mengnankkzhou@gmail.com>
2025-05-18 16:48:54 +0800

    项目结构搭建

---
+++ crmeb/crmeb-admin/src/main/java/com/mengnankk/admin/CrmebAdminApplication.java
@@ -0,0 +1,10 @@
+package com.mengnankk.admin;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+
+public class CrmebAdminApplication {
+    public static void main(String[] args) {
+    }
+}
